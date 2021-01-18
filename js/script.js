ee77cc4d84112f578646c6440bf99b2c2b08db4d
hyenial <harun.yenial@gmail.com>
2021-01-17 23:52:40 -0500

    ‘your_message’

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+function loadData() {
+
+    var $body = $('body');
+    var $wikiElem = $('#wikipedia-links');
+    var $nytHeaderElem = $('#nytimes-header');
+    var $nytElem = $('#nytimes-articles');
+    var $greeting = $('#greeting');
+
+    // clear out old data before new request
+    $wikiElem.text("");
+    $nytElem.text("");
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').var();
+    var address = streetStr + ', ' + cityStr;
+    
+    $greeting.text('So, you want to live at' + address + '?');
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + address + '';
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+
+
+    // load streetview
+
+    // YOUR CODE GOES HERE!
+
+    return false;
+};
+
+$('#form-container').submit(loadData);
+
+// loadData();
